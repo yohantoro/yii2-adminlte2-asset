7b6c7013138c9ceb05ab2fc01b58cc5f233c9ebd
yohantoro <yosephyh@akakom.ac.id>
2023-12-05 11:42:43 +0700

    update aliases

--- tests/_app/config/local.php
+++ tests/_app/config/local.php
@@ -6,9 +6,9 @@ return [
         'repo' => '/repo',
         'tests/app' => '@repo/tests/_app',
         'dmstr' => '/repo',
-        '@dmstr/web' => '/repo/web',
-        '@dmstr/widgets' => '/repo/widgets',
-        '@dmstr/helpers' => '/repo/helpers',
+        '@yohantoro/adminlte2/web' => '/repo/web',
+        '@yohantoro/adminlte2/widgets' => '/repo/widgets',
+        '@yohantoro/adminlte2/helpers' => '/repo/helpers',
     ],
     'controllerNamespace' => 'tests\app\controllers',
     'components' => [
