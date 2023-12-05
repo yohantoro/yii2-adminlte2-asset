f9b1eba7a208d47c756b5b8f528f68c629a49172
yohantoro <yosephyh@akakom.ac.id>
2023-12-05 10:57:06 +0700

    revert

--- web/AdminLteAsset.php
+++ web/AdminLteAsset.php
@@ -1,6 +1,7 @@
 <?php
-namespace dmstr\adminlte\web;
+namespace dmstr\web;
 
+use yii\base\Exception;
 use yii\web\AssetBundle as BaseAdminLteAsset;
 
 /**
@@ -11,14 +12,38 @@ class AdminLteAsset extends BaseAdminLteAsset
 {
     public $sourcePath = '@vendor/almasaeed2010/adminlte/dist';
     public $css = [
-        'css/adminlte.min.css',
+        'css/AdminLTE.min.css',
     ];
     public $js = [
         'js/adminlte.min.js'
     ];
     public $depends = [
+        'rmrevin\yii\fontawesome\AssetBundle',
         'yii\web\YiiAsset',
         'yohantoro\yii2\bootstrap3\BootstrapAsset',
         'yohantoro\yii2\bootstrap3\BootstrapPluginAsset',
     ];
+
+    /**
+     * @var string|bool Choose skin color, eg. `'skin-blue'` or set `false` to disable skin loading
+     * @see https://almsaeedstudio.com/themes/AdminLTE/documentation/index.html#layout
+     */
+    public $skin = '_all-skins';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        // Append skin color file if specified
+        if ($this->skin) {
+            if (('_all-skins' !== $this->skin) && (strpos($this->skin, 'skin-') !== 0)) {
+                throw new Exception('Invalid skin specified');
+            }
+
+            $this->css[] = sprintf('css/skins/%s.min.css', $this->skin);
+        }
+
+        parent::init();
+    }
 }
